a32ce8b8392a3ccbbaa1b05c8cf82498a9c2b1c7
Mohammad Hossein Nematiyan Jelodar <nematmhn@gmail.com>
2025-12-16 13:54:44 +0000

    Clear repository for fresh start

--- frontend/src/views/manager/Dashboard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>داشبورد مدیر گروه - در حال توسعه</div>
-</template>
-
-<script setup>
-import { useAuthStore } from '../../stores/auth'
-const authStore = useAuthStore()
-</script>
--- frontend/src/views/professor/Dashboard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>داشبورد استاد - در حال توسعه</div>
-</template>
-
-<script setup>
-import { useAuthStore } from '../../stores/auth'
-const authStore = useAuthStore()
-</script>
